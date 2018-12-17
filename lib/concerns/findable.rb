85532470d7a509e007d6909be7f9e8423759625f
Stephen Olson <stephenolson@gmail.com>
2018-12-17 23:36:22 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,7 @@
 module Findable
   
-   def find_by_name(name)
-    self.all.detect{|a| a.name == name}
-  end
-  
-   def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+  def find_by_name(name)
+    self.all.detect{|a| a.name}
   end
   
 end
