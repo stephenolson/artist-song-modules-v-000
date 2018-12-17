8b8553c4d048da39a90517e907ec2f80dd99b8c9
Stephen Olson <stephenolson@gmail.com>
2018-12-17 23:35:25 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Findable
   
-   def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+   def find_by_name(name)
+    .self.detect{|a| a.name == name}
   end
   
    def self.find_by_name(name)
